d8de1529ed9a380c6fc2c6ffdca7fd9c535e2bc0
Stefan <stefan.dekanovic2@gmail.com>
2020-08-07 12:23:17 +0200

    popravek analize

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -26,6 +26,7 @@ statSignificence <- as.logical(coef(summary(model1))[, 4] < 0.05)
 statSignificence <- names(model1$coefficients[statSignificence])
 
 df2 <- as.data.frame(coef(summary(model1)))
+rownames(df2)[rownames(df2) == "`Velikost kredita`"] = "Velikost kredita"
 
 napoved <- model1 %>% predict(testna, type = "response")
 napoved[napoved > 0.5] = 1
@@ -33,12 +34,3 @@ napoved[napoved <= 0.5] = 0
 
 tocnost <- mean(napoved == testna$Ocena)
 
-# podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-#                                 gostota.naselij=naselja/povrsina) %>%
-#   left_join(povprecja, by="obcina")
-# row.names(podatki) <- podatki$obcina
-# podatki$obcina <- NULL
-# 
-# # Število skupin
-# n <- 5
-# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
